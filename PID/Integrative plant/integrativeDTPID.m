9d84297adff2a04a594e3663d734fd7082137c04
Juan Moreira <juanmoreira@alu.ufc.br>
2023-06-09 02:32:17 -0300

    Robustness indice

--- PID/Integrative plant/integrativeDTPID.m
+++ PID/Integrative plant/integrativeDTPID.m
@@ -11,12 +11,15 @@ s = tf('s');
 %% At least, we need to use a reference filter do attenuate the overshoot.
 % Our filter is similar to 2DOF one being: F = (1+(b*Tif*s)+(c*Tif*Tdf*s*s))/(1+(Tif*s)+(Tif*Tdf*s*s))
 % Kcf = Kc+Ko; Tff =Tf; Tif = (1+(Ko/Kc))*Ti; Tdf = Kc*Tf+Ko*Tf/(Kc+ko);
+%% Note: In lag dominant process, being that T >> L, we can approximate P to be integrative
 %% application
 P = 2/((1+s)*(1+0.5*s)*s*(1+0.1*s));
 P.OutputDelay = 5;
 % de Pn temos:
 Kv = 2; 
 L = 6.7;
+Pn = Kv/s;
+Pn.OutputDelay = L;
 gamma = 0.2;
 b = 1;
 c = 1;
@@ -33,7 +36,7 @@ C = Kc*(1+(1/(Ti*s))+Td*s)*(1/(1+s*Tf));
 F = (1+(b*Ti*s)+(c*Ti*Td*s*s))/(1+(Ti*s)+(Ti*Td*s*s));
 Tinit = 0;
 Tsim = 150;
-Tdist = 50;
+Tdist = 75;
 AmpDist = -0.05;
 
 sim_02 = sim('idealIntegrative2DOF.slx');
@@ -57,11 +60,22 @@ plot(sim_03.u, '--k', 'linewidth', 1)
 grid on
 axis tight
 legend('Output, u(t) 0.2','Output u(t) 0.3', 'location', 'best') 
+Ts = 1;
+wz = logspace(-1,log10(pi/Ts),1000);
+dP = abs(freqresp((P - Pn),wz))./abs(freqresp(Pn,wz));
+dP = squeeze(dP(1,1,:));
 
+Ir = abs(freqresp((C*P + 1),wz))./abs(freqresp(C*P,wz));
+Ir = squeeze(Ir(1,1,:));
 
-
-
-
-
+figure
+semilogx(wz, dP, '--k', 'linewidth', 2)
+hold on
+semilogx(wz, Ir, 'b', 'linewidth', 1.5)
+grid on
+axis tight
+xlabel('Frequency, rad/s')
+ylabel('Magnitude')
+legend('Uncertainty, dP','Robustness index', 'location', 'best')
 
 
